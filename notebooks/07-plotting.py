1cea30286466a4f45603809eae10792dcc337fa9
jan-vavrin <147444406+jan-vavrin@users.noreply.github.com>
2026-06-05 14:44:06 +0200

    fix: typos & small mistakes (#11)
    * Update 05-files.py
    
    Print newlines separately, otherwise you get a space at the beginning of the line.
    
    Small typos.
    
    * Update 06-numpy.py
    * Update 07-plotting.py
    * Update 01-notebooks.py
    * Update 02-variables.py
    * Update 03-functions.py
    * Update 04-flow.py

--- notebooks/07-plotting.py
+++ notebooks/07-plotting.py
@@ -42,7 +42,7 @@ def _():
     In `matplotlib` we build up the elements on a canvas. We have two major objects to interact with:
 
     - **the figure** (`fig`): this is the canvas; it controls the size, the background,  final export etc.
-    - **the axes**(`ax`): this is the actual drawing area; you can have many axes in one figure (think of subplots) and it controls elements related to the plot itself.
+    - **the axes** (`ax`): this is the actual drawing area; you can have many axes in one figure (think of subplots) and it controls elements related to the plot itself.
     """)
     return
 
@@ -72,10 +72,10 @@ def _():
             label="Points"
         )
 
+        # Draw a dashed line on the axes, in red
         # The above is equivalent to shorthand notation:
         # ax.plot(x, y, "ko", label="Points")
 
-        # Draw a fashed line on the axes, in red
         # Could use data from scipy.optimize here
         ax.plot(
             x,
@@ -96,7 +96,7 @@ def _():
     mo.md(r"""
     ## Adding labels and legends
 
-    This looks already quite good. But a plot without labels is not really "data". Let's look at how we can annotate the plot further.
+    This already looks quite good. But a plot without labels is not really "data". Let's look at how we can annotate the plot further.
 
     > **Note**: in chemistry we often forego the legend and annotate the figure fully in the caption. This could say:
     >
@@ -127,7 +127,7 @@ def _(np, plt):
         ax.plot(
             x, y, 
             color="purple",
-            linewidth="1",
+            linewidth=1.0,
             label="Oscillation"
         )
 
@@ -147,7 +147,7 @@ def _():
     mo.md(r"""
     ## LaTeX in labels
 
-    In sciences, we often have comlicated labels in our plots. You can use [LaTeX](https://en.wikipedia.org/wiki/LaTeX) for formatting the labels in `matplotlib`.
+    In sciences, we often have complicated labels in our plots. You can use [LaTeX](https://en.wikipedia.org/wiki/LaTeX) for formatting the labels in `matplotlib`.
     """)
     return
 
@@ -187,7 +187,7 @@ def _(np, plt):
         )
 
         # LaTeX for math/units
-        # Not that we need to use "raw" strings, as LaTeX contains {}
+        # Not that we need to use "raw" strings, as LaTeX contains "\"
         ax.set_xlabel(r"Time, $t$ / min")
         ax.set_ylabel(r"Concentration, $c$ / mol$\cdot$l$^{-1}$")
 
@@ -237,7 +237,7 @@ def _(plt):
 @app.cell(hide_code=True)
 def _():
     mo.md(r"""
-    We can even go all crazy with `subplot_mosaic()` - less common in chemistry, but belowed by some fields like bioinformatics. It takes a text-based map describing our layout. As it gets more and more complicated, you might be better off aligning those plots in a separate graphics editor (like open-source vector editor [Inkscape](https://inkscape.org/)).
+    We can even go all crazy with `subplot_mosaic()` - less common in chemistry, but belowed by some fields like bioinformatics. It takes a text-based map describing our layout. As it gets more and more complicated, you might be better off aligning those plots in a separate graphics editor (like the open-source vector editor [Inkscape](https://inkscape.org/)).
     """)
     return
 
